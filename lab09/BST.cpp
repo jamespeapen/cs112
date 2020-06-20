51967ea686b2b1bdae378490d111f63e38b32d59
James Eapen <jamespeapen@gmail.com>
2020-06-19 20:27:28 -0400

    move to main repo

---
+++ lab09/BST.cpp
@@ -0,0 +1,64 @@
+/* BST.cpp defines binary search tree methods.
+ * Joel Adams, for CS 112 at Calvin College.
+ * Student Name:
+ * Date:
+ */
+ 
+#include "BST.h"
+
+
+BST::~BST() {
+	delete myRoot;
+	myRoot = NULL;
+	myNumItems = 0;
+}
+
+BST::Node::Node(const Item& it) {
+   myItem = it;
+   myLeft = NULL;
+   myRight = NULL;
+}
+
+BST::Node::~Node() {
+	delete myLeft;
+	delete myRight;
+}
+
+bool BST::isEmpty() const {
+	return myNumItems == 0;
+}
+
+unsigned BST::getNumItems() const {
+	return myNumItems;
+}
+
+void BST::traversePreorder() {
+	if ( !isEmpty() ) {
+		myRoot->traversePreorder();
+	}
+}
+
+void BST::Node::traversePreorder() {
+	processItem();
+	if (myLeft != NULL) {
+		myLeft->traversePreorder();
+	}
+	if (myRight != NULL) {
+		myRight->traversePreorder();
+	}
+}
+
+void BST::Node::processItem() {
+		cout << ' ' << myItem;
+}
+
+void BST::Node::traversePostorder() {
+	if (myLeft != NULL) {
+		myLeft->traversePostorder();
+	}
+	if (myRight != NULL) {
+		myRight->traversePostorder();
+	}
+	processItem();
+}
+
